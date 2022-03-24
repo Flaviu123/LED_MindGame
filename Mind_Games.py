39a50e0848d9c1eb42a027868c262600879d5d19
YvenKl <94714197+YvenKl@users.noreply.github.com>
2022-03-24 13:59:51 +0100

    Update Mind_Games.py

--- Mind_Games.py
+++ Mind_Games.py
@@ -6,10 +6,13 @@ import random
 class Settings():
     num_io = 2
     gameover = False
+    time = 2
+    generate_sequencecheck = 0
     sequence_ledlength = 0
     sequence_showcheck = 1
     show_sequence = 1
     input_check = 0
+    led_length = 5
     LED1 = LED(17)
     LED2 = LED(27)
     LED3 = LED(22)
@@ -33,98 +36,86 @@ class Game():
         pass
 
 
-class Level():
-    def __init__(self):
-        self.level_ind = 0
-        self.led_length = 2
-        self.time = 0
-        self.sequence = None
-
-    def next_level(self):
-        self.sequence = Sequence(self.led_length, self.time)
-
-
-    def reset_level(self):
-        pass
-
 
 class Sequence():
-    def __init__(self, led_length, time):
-        self.led_length = led_length
-        self.time = time
+    def __init__(self):
         self.sequence = []
-        self.playerinput = []
+        self.playerinputlist = []
 
-    def generate_sequence(self, led_length):
-        for i in range(0, led_lenght):
+    def generate_sequence(self):
+        for i in range(0, Settings.led_length):
             add_s = random.uniform(1, Settings.num_io)
             self.sequence.append(add_s)
+            Settings.generate_sequencecheck += 1
+            if Settings.generate_sequencecheck == Settings.led_length:
+               self.show_sequence(Settings.time)
 
 
     def show_sequence(self, time):
-        if Settings.sequence_showcheck == Settings.show_sequence:
+        for i in range(0, Settings.led_length + 1):
             if self.sequence[Settings.show_sequence - 1] == 1:
                 LED1.on()
+                Settings.show_sequence += 1
                 sleep(time)
                 LED1.off()
 
             elif self.sequence[Settings.show_sequence - 1] == 2:
                 LED2.on()
+                Settings.show_sequence += 1
                 sleep(time)
                 LED2.off()
 
             elif self.sequence[Settings.show_sequence - 1] == 3:
                 LED3.on()
+                Settings.show_sequence += 1
                 sleep(time)
                 LED3.off()
 
             elif self.sequence[Settings.show_sequence - 1] == 4:
                 LED4.on()
+                Settings.show_sequence += 1
                 sleep(time)
                 LED4.off()
 
             elif self.sequence[Settings.show_sequence - 1] == 5:
                 LED5.on()
+                Settings.show_sequence += 1
                 sleep(time)
                 LED5.off()
 
-        if len(self.sequence) == Settings.sequence_showcheck:
-            Settings.show_sequence = 1
-            Settings.sequence_showcheck = 1
-            self.input_sequence()
-
-        else:
-            Settings.show_sequence += 1
-            Settings.sequence_showcheck += 1
-            self.show_sequence()
+            elif len(self.sequence) == Settings.show_sequence - 1:
+                Settings.show_sequence = 1
+                self.input_sequence()
 
 
     def player_input(self):
         if Settings.Button1.is_pressed:
-            self.playerinput.append(1)
+            self.playerinputlist.append(1)
             self.check_sequence()
 
-        if Settings.Button2.is_pressed:
-            self.playerinput.append(2)
+        elif Settings.Button2.is_pressed:
+            self.playerinputlist.append(2)
             self.check_sequence()
 
-        if Settings.Button3.is_pressed:
-            self.playerinput.append(3)
+        elif Settings.Button3.is_pressed:
+            self.playerinputlist.append(3)
             self.check_sequence()
 
-        if Settings.Button4.is_pressed:
-            self.playerinput.append(4)
+        elif Settings.Button4.is_pressed:
+            self.playerinputlist.append(4)
             self.check_sequence()
 
-        if Settings.Button5.is_pressed:
-            self.playerinput.append(5)
+        elif Settings.Button5.is_pressed:
+            self.playerinputlist.append(5)
             self.check_sequence()
-            
-            
+
+        elif len(self.playerinputlist) == len(self.sequence):
+            pass #Whileschleife wird unterbrochen
+
 
 
     def check_sequence(self):
-        if self.sequence[Settings.input_check] == self.playerinput[Settings.input_check]:
+        if self.sequence[Settings.input_check] == self.playerinputlist[Settings.input_check]:
             print("Richtige LED gemerkt!")
             Settings.input_check += 1
         else:
